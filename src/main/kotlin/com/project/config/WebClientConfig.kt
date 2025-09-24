80a2529db48ab2b91ff081e20519787b4cdc559f
“abolade” <aboladelawal2019@gmail.com@gmail.com>
2025-09-24 06:46:04 +0100

    Configure WebClient to support a higher in-memory size limit for large payload handling

--- src/main/kotlin/com/project/config/WebClientConfig.kt
+++ src/main/kotlin/com/project/config/WebClientConfig.kt
@@ -2,6 +2,7 @@ package com.project.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -9,7 +10,13 @@ class WebClientConfig {
 
     @Bean
     fun webClient(): WebClient {
+        val strategies = ExchangeStrategies.builder()
+            .codecs { config ->
+                config.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)
+            }
+            .build()
         return WebClient.builder()
+            .exchangeStrategies(strategies)
             .build()
     }
 }
